ee19f238fb036e17e7f0e9cec94e3ce04f3c1544
eslam <eslam@ESLAMAYMAN>
2024-03-23 01:14:32 +0200

    - Generate Architecture work for the project

---
+++ Domain/Entities/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class BaseModel
+    {
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public DateTime? LastUpdatedOn { get; set; }
+    }
+}
---
+++ Domain/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Country: BaseModel
+    {
+        public int Id { get; set; }
+        [MaxLength(150)]
+        public string Name { get; set; } = null!;
+    }
+}
