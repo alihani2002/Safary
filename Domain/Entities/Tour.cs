4b0b79fdbfe359ddcf7e58026d117a6e8ab7922e
Moustafa Batt <MoustafaMohammedbatt>
2024-03-23 17:23:42 +0200

    Add Tour's Properties

--- Domain/Entities/Tour.cs
+++ Domain/Entities/Tour.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    public class Tour
+    public class Tour : BaseModel
     {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(150, ErrorMessage = "Username cannot exceed 150 characters")]
+        public string Name { get; set; } = null!;
+        
+        public decimal Price { get; set; }
+        
+        [Required]
+        public string Description { get; set; } = null!;
+        
+        [Range(1, 23, ErrorMessage = " Duration Must be more Than 1 Hour less Than 24 Hours")]
+        public int Duration { get; set; }
+        
+        public DateTime StartDate { get; set; }
+        
+        public DateTime EndDate { get; set; }
+
     }
 }
