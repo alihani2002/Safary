818ba6404c880710715da5db8e2a893ab4e5642a
alyhani2024 <ahani3012@gmail.com>
2024-03-24 15:17:52 +0200

    Add Blog class

---
+++ Domain/Entities/Blog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Blog : BaseModel
+    {
+        public int Id { get; set; }
+
+        [MaxLength(150, ErrorMessage = "Username cannot exceed 150 characters")]
+        public string Name { get; set; } = null!;
+        [Required]
+        public string Description { get; set; } = null!;
+        public DateTime Time { get; set; }
+        public string Content { get; set; } = null!;
+
+    }
+}
