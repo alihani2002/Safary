d2c04fbc6c80aa87112e9e92e2c537252e2773f5
eslam <eslam@ESLAMAYMAN>
2024-04-18 15:39:57 +0200

    - add ibaseRepository pattern and unit of work with implementation - add order by class const

---
+++ Domain/Repositories/IBaseRepository.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Consts;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace Domain.Repositories
+{
+	public interface IBaseRepository<T> where T : class
+	{
+		IEnumerable<T> GetAll(bool withNoTracking = true);
+		IQueryable<T> GetQueryable();
+		T? GetById(int id);
+		T? Find(Expression<Func<T, bool>> predicate);
+		T? Find(Expression<Func<T, bool>> predicate, string[]? includes = null);
+		T? Find(Expression<Func<T, bool>> predicate,
+				Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null);
+		IEnumerable<T> FindAll(Expression<Func<T, bool>> predicate, int? skip = null, int? take = null,
+			Expression<Func<T, object>>? orderBy = null, string? orderByDirection = OrderBy.Ascending);
+		IEnumerable<T> FindAll(Expression<Func<T, bool>> predicate,
+			Expression<Func<T, object>>? orderBy = null, string? orderByDirection = OrderBy.Ascending);
+		IEnumerable<T> FindAll(Expression<Func<T, bool>> predicate, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+			Expression<Func<T, object>>? orderBy = null, string? orderByDirection = OrderBy.Ascending);
+		T Add(T entity);
+		IEnumerable<T> AddRange(IEnumerable<T> entities);
+		void Update(T entity);
+		void Remove(T entity);
+		void RemoveRange(IEnumerable<T> entities);
+		void DeleteBulk(Expression<Func<T, bool>> predicate);
+		bool IsExists(Expression<Func<T, bool>> predicate);
+		int Count();
+		int Count(Expression<Func<T, bool>> predicate);
+		int Max(Expression<Func<T, bool>> predicate, Expression<Func<T, int>> field);
+	}
+}
---
+++ Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Repositories;
+using Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+	public class UnitOfWork: IUnitOfWork
+	{
+		private readonly ApplicationDbContext _context;
+
+		public UnitOfWork(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public int Complete()
+		{
+			return _context.SaveChanges();
+		}
+	}
+}
