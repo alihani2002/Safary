693f1fe1d5a9c9d1cf46456e8b069b9f92850df8
Moustafa Batt <MoustafaMohammedbatt>
2024-04-26 00:32:42 +0200

    add project reference to add controller

---
+++ Presentations/Controllers/TourDayController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Safary.Properties
+{
+    public class TourDayController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
